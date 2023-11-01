83e223da9ec91101d48d3dcc3a008db67630e6f6
Federico Vicente <federicovicente@gmail.com>
2023-11-01 18:13:10 -0300

    a

--- js/script.js
+++ js/script.js
@@ -73,11 +73,10 @@ wrapper.addEventListener("mouseleave", autoPlay);
 
 
 
-
 let loguin
 
 function verificarLoguin(){
-   let loguinEstado = document.getElementById('iniciar')
+   let loguinEstado = document.getElementById('btniniciarsesion')
 
    if (loguin === 1){
        loguinEstado.textContent = 'Bienvenido'
@@ -90,32 +89,19 @@ function verificarLoguin(){
  let passUsuario
 
 function IniciarSesion(){
-    nombreUsuario = document.getElementById('usuario').value
-    passUsuario = document.getElementById('contrasenia').value
-    if (nombreUsuario === 'federicov' && passUsuario === '123'){
+    nombreUsuario = document.getElementById('emailuser').value
+    passUsuario = document.getElementById('contraseniauser').value
+    if (nombreUsuario === 'federicov@gmail.com' && passUsuario === '123'){
         loguin = 1
         cerrarLoguin()
         alert('Bienvenido')
      } else {
-        alert('Usuario o contraseña incprrectos')
+        alert('Usuario o contraseña incorrectos')
         loguin = 0
      }
      verificarLoguin()
 }
 
-function IniciarSesion2(){
-   nombreUsuario2 = document.getElementById('InputEmail1').value
-   passUsuario2 = document.getElementById('InputPassword1').value
-   if (nombreUsuario2 == 'federicov@gmail.com' && passUsuario2 === '123'){
-       loguin = 1
-       cerrarLoguin()
-       alert('Bienvenido')
-    } else {
-       alert('Usuario o contraseña incprrectos')
-       loguin = 0
-    }
-    verificarLoguin()
-}
 
 function cerrarLoguin() {
    $('#modalLoguin').modal('hide');
